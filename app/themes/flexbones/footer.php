069dae5333929a4e0cf4e66bee4d5e3cc1ce1ad1
Olly Pittaway <olly.pittaway@codebluegroup.co.uk>
2017-12-11 14:55:39 +0000

    Update to where jQuery is included to avoid  `$ is not defined error`

--- app/themes/flexbones/footer.php
+++ app/themes/flexbones/footer.php
@@ -1,5 +1,4 @@
 <?php wp_footer(); ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/scripts.min.js"></script>
 
 </body>
--- app/themes/flexbones/header.php
+++ app/themes/flexbones/header.php
@@ -5,11 +5,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" href="<?php assets('imgs'); ?>/favicon.ico" type="image/x-icon">
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/min-style.css">
+
 <!--[if (lte IE 9)]>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js" ></script>
 <![endif]-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript">
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
