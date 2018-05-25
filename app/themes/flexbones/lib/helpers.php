e27bc717d4e2556418a03e304d30ed3997c40983
Olly Pittaway <olly.pittaway@codebluegroup.co.uk>
2018-05-25 09:46:32 +0100

    Added a script to enqueue a custom version of jQuery as standard

--- app/themes/flexbones/lib/helpers.php
+++ app/themes/flexbones/lib/helpers.php
@@ -12,3 +12,9 @@ function get_assets($subdir){
 function assets($subdir){
     echo get_stylesheet_directory_uri() .'/assets'.($subdir ? '/'. $subdir : '');
 }
+
+function custom_jquery() {
+	wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'custom_jquery');
